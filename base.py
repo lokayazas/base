2aad8142ea41c04ef768acfe745e55a639da6000
lokayazas <68589229+lokayazas@users.noreply.github.com>
2020-08-08 23:59:51 +0800

    Create base.py

---
+++ base.py
@@ -0,0 +1,30 @@
+import IPython
+from IPython.utils import io
+from IPython.display import display,HTML,clear_output
+import subprocess
+
+import os
+from getpass import getpass
+import urllib
+
+def printh(t,dynamic=False):
+  if(dynamic):
+    time.sleep(1)
+    clear_output(wait=True)
+  display(IPython.display.HTML("<p>"+t+"</p>"))
+
+def getapp():
+  printh("User name: ")
+  user = input('')
+  printh("Password: ")
+  password = getpass('')
+  password = urllib.parse.quote(password) # your password is converted into url format
+  printh("Repo name: ")
+  repo_name = input('')
+
+  cmd_string = 'git clone https://{0}:{1}@github.com/{0}/{2}.git'.format(user, password, repo_name)
+
+  os.system(cmd_string)
+  cmd_string, password = "", "" # removing the password from the variable
+  os.system('cd '+repo_name)
+  os.system('python '+repo_name)
